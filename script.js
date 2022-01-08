3f740ef66820bae871f2014aaac4d8314c130a51
Girish Madnani <girishmadnani05@gmail.com>
2022-01-08 00:27:05 +0000

    length of password

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+
+  let smLetter = 'abcdefghijklmnopqrstuvwxyz'
+  let laLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let specialChar = '!@#$%^&*()_+-={}|[]:"<>?;,./`~'
+  let numbers = '1234567890'
+  let allowedChar = ''
+  let confirmPassword = ''
+
+  var passwordLength = prompt("Select number of characters you would like your password to be. \nMust be between 8 - 128 characters.");
+
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) || passwordLength == 0){
+    alert ("Invalid choice, Please try again");
+    return;
+  }
+  console.log(passwordLength)
+}
